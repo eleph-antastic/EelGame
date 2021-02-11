debc55ab553ba8391a82dac2638ace14f97ef561
Julia Sousa <60710752+eleph-antastic@users.noreply.github.com>
2021-02-10 17:01:00 -0700

    Buttons for Menus
    -buttons working for pause and play and quit
    -main menu created
    -cube test object inserted to test timescale in pause

--- UnityProgramming_EelGame/Assets/Scripts/Buttons.cs
+++ UnityProgramming_EelGame/Assets/Scripts/Buttons.cs
@@ -1,18 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject pauseScreen;
+
+    //When Clicking the Pause 
+    public void pauseButton()
     {
-        
+        //pause game
+        pauseScreen.SetActive(true);
+        Time.timeScale = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void resumeButton()
     {
-        
+        //resume from paused
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void menuButton()
+    {
+        //go to the main menu
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void quitButton()
+    {
+        //quits game
+        Application.Quit();
+    }
+
+    public void playButton()
+    {
+        //on MainMenu, Play button to start game. Also the Play Again button after losing
+        SceneManager.LoadScene("gameScene");
     }
 }
