ccd10f3037898c89b44ce615423cc6bd6df4e2ab
Julia Sousa <60710752+eleph-antastic@users.noreply.github.com>
2021-02-16 22:47:20 -0700

    PlayerJump
    -petController script added
    -Jumping and Crouching basics working

---
+++ UnityProgramming_EelGame/Assets/Scripts/petController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class petController : MonoBehaviour
+{
+    public GameObject player;
+
+    public float jumpHeight;
+    public float duckAmount;
+
+    public bool atStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        atStart = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("up") || Input.GetKeyDown(KeyCode.W) && atStart) //jumps up
+        {
+            atStart = false;
+            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + jumpHeight, player.transform.position.z);
+        }
+        else if (Input.GetKeyUp("up") || Input.GetKeyUp(KeyCode.W) && !atStart) //moves player back to start position
+        {
+            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - jumpHeight, player.transform.position.z);
+            atStart = true;
+        }
+        else if (Input.GetKeyDown("down") || Input.GetKeyDown(KeyCode.S) && atStart) //ducks down
+        {
+            atStart = false;
+            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - duckAmount, player.transform.position.z);
+        }
+        else if (Input.GetKeyUp("down") || Input.GetKeyUp(KeyCode.S) && !atStart) //moves player back to start position
+        {
+            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + duckAmount, player.transform.position.z);
+            atStart = true;
+        }
+    }
+}
