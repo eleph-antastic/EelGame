b3a458a30dd92c85386b2da3453c3b4ff29c66cb
Julia Sousa <60710752+eleph-antastic@users.noreply.github.com>
2021-02-18 21:14:44 -0700

    Instructions Panel
    - buttons added
    -instructions added
    -sound effects
    -audio source added

--- UnityProgramming_EelGame/Assets/Scripts/Buttons.cs
+++ UnityProgramming_EelGame/Assets/Scripts/Buttons.cs
@@ -6,13 +6,24 @@ using UnityEngine.SceneManagement;
 public class Buttons : MonoBehaviour
 {
     public GameObject pauseScreen;
+    AudioSource audioPlayer;
     public AudioClip paused;
+    public GameObject instructionPanel;
+    public AudioClip beginMusic;
 
+    void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+        instructionPanel.SetActive(true);
+        Time.timeScale = 0;
+    }
     //When Clicking the Pause 
     public void pauseButton()
     {
         //pause game
-        AudioSource.PlayClipAtPoint(paused, transform.position);
+        audioPlayer.clip = paused;
+        audioPlayer.volume = 0.5f;
+        audioPlayer.Play();
         pauseScreen.SetActive(true);
         Time.timeScale = 0;
     }
@@ -41,5 +52,14 @@ public class Buttons : MonoBehaviour
     {
         //on MainMenu, Play button to start game. Also the Play Again button after losing
         SceneManager.LoadScene("gameScene");
+    }
+    public void startGameButton()
+    {
+        //starts games
+        audioPlayer.clip = beginMusic;
+        audioPlayer.volume = 1.0f;
+        Time.timeScale = 1;
+        instructionPanel.SetActive(false);
+        audioPlayer.Play();
     }
 }
---
+++ UnityProgramming_EelGame/Assets/Scripts/instructionPanelScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instructionPanelScript : MonoBehaviour
+{
+
+    public GameObject instructionPanel;
+    public AudioClip beginMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instructionPanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void playButton()
+    {
+        //starts game
+        Time.timeScale = 1;
+        instructionPanel.SetActive(false);
+        AudioSource.PlayClipAtPoint(beginMusic, transform.position);
+    }
+}
